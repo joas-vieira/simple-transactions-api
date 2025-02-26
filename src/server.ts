930fbfb113b8c909afd9f3031e79f1172de12bf3
joas-vieira <joas.vieira.dev@gmail.com>
2025-02-26 18:53:27 -0300

    chore: add eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 const server = fastify();
 
-server.get("/", () => {
-  return { hello: "world" };
+server.get('/', () => {
+  return { hello: 'world' };
 });
 
-server.listen({
-  port: 3000
-}).then(() => {
-  console.log("Server is running on port 3000");
+server.listen({ port: 3000 }).then(() => {
+  console.log('Server is running on port 3000');
 });
