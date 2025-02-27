d5c07dc64e21acca5e2fbcd2bc4c9f3ee39aeaaa
joas-vieira <joas.vieira.dev@gmail.com>
2025-02-27 07:32:58 -0300

    feat: config migrations

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,15 @@
-import { knex as Knex } from 'knex';
+import { knex as knexLib, Knex } from 'knex';
 
-export const knex = Knex({
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './tmp/db.sqlite'
+    filename: './db/db.sqlite'
   },
-  useNullAsDefault: true
-});
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations'
+  }
+};
+
+export const knex = knexLib(config);
